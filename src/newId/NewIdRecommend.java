063c9665e0b6b027b22b8902202f05308b61747e
bsh6463 <bsh9510@gmail.com>
2021-12-04 22:36:52 +0900

    211204 Number And Word

--- src/newId/NewIdRecommend.java
+++ src/newId/NewIdRecommend.java
@@ -6,16 +6,43 @@ public class NewIdRecommend {
     public String solution(String new_id) {
         char[] cannotUse = {'~','!','@','#','$','%','^','&','*','(',')','=','+','[','{',']','}',':','?',',','<','>','/'};
         char[] newId = new_id.toCharArray();
+        
 
         //1단계 : 대문자 -> 소문자.
-        for (int i=0; i< newId.length; i++){
+        toLowerCase(newId);
+        new_id = new String(newId);
+
+        //2단계 특수문자 제거 - _ . 제외한 특수문자 제거
+        new_id = removeSpecialChar(new_id, cannotUse);
+
+        //3단계 .가 2개 이상인 경우 .으로 변환.
+        new_id = changeDotsToDot(new_id);
+
+        //4단계 .가 처음이나 끝에 있으며 삭제.
+        new_id = removeFistOrLastDot(new_id);
+
+        //5단계
+        new_id = changeEmptyToA(new_id);
+
+        //6단계
+        new_id = noGreaterThan16(new_id);
+
+
+        //7단계
+        new_id = notShorterThan3(new_id);
+
+        return new_id;
+    }
+
+    private void toLowerCase(char[] newId) {
+        for (int i = 0; i< newId.length; i++){
             if ((int) newId[i] >= 65 && (int) newId[i] <= 90){
                 newId[i] += 32;
             }
         }
-        new_id = new String(newId);
+    }
 
-        //2단계 특수문자 제거 - _ . 제외한 특수문자 제거
+    private String removeSpecialChar(String new_id, char[] cannotUse) {
         for (char c : cannotUse) {
             new_id = new_id.replace(c, ' ');
         }
@@ -24,26 +51,34 @@ public class NewIdRecommend {
         while (new_id.contains(" ")){
             new_id = new_id.replace(" ", "");
         }
+        return new_id;
+    }
 
-        //3단계 .가 2개 이상인 경우 .으로 변환.
+    private String changeDotsToDot(String new_id) {
         while (new_id.contains("..")){
             new_id = new_id.replace("..", ".");
         }
+        return new_id;
+    }
 
-        //4단계 .가 처음이나 끝에 있으며 삭제.
+    private String removeFistOrLastDot(String new_id) {
         if (new_id.startsWith(".")){
             new_id = new_id.substring(1, new_id.length());
         }
         if (new_id.endsWith(".")){
             new_id = new_id.substring(0, new_id.length() - 1);
         }
+        return new_id;
+    }
 
-        //5단계
-        if (new_id.isBlank() || new_id.isEmpty()){
+    private String changeEmptyToA(String new_id) {
+        if (new_id.isBlank() || new_id.isEmpty()) {
             new_id = "a";
         }
+        return new_id;
+    }
 
-        //6단계
+    private String noGreaterThan16(String new_id) {
         if (new_id.length() >= 16){
             //15자만 짜르기
             new_id = new_id.substring(0, 15);
@@ -51,9 +86,10 @@ public class NewIdRecommend {
                 new_id = new_id.substring(0, 14);
             }
         }
+        return new_id;
+    }
 
-
-        //7단계
+    private String notShorterThan3(String new_id) {
         if (new_id.length() <=2 ){
             while (new_id.length() < 3){
                 int index = new_id.length()-1;
@@ -61,8 +97,6 @@ public class NewIdRecommend {
                 new_id = new_id + c;
             }
         }
-
-
         return new_id;
     }
 
---
+++ src/numbersandword/NumberAndWord.java
@@ -0,0 +1,47 @@
+package numbersandword;
+
+
+import java.util.HashMap;
+
+public class NumberAndWord {
+
+    public int solution(String s) {
+        HashMap<String, String> translator = init();
+
+        for (String word : translator.keySet()) {
+            if (s.contains(word)){
+                s = s.replace(word, translator.get(word));
+            }
+        }
+
+        return Integer.parseInt(s);
+    }
+
+    private HashMap<String, String> init() {
+        HashMap<String, String> translator = new HashMap<>();
+        translator.put("zero","0");
+        translator.put("one","1") ;
+        translator.put("two","2") ;
+        translator.put("three","3");
+        translator.put("four","4");
+        translator.put("five","5");
+        translator.put("six","6") ;
+        translator.put("seven","7");
+        translator.put("eight","8");
+        translator.put("nine","9");
+        return translator;
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "one4seveneight";
+
+       NumberAndWord numberAndWord = new NumberAndWord();
+
+        int answer = numberAndWord.solution(s1);
+        System.out.println("======answer========");
+        System.out.println(answer);
+
+    }
+
+}
