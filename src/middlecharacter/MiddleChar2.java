ed1bf6bdf14c5200bae01afd59d00676d41ccccd
bsh6463 <bsh9510@gmail.com>
2021-12-12 15:10:08 +0900

    211212 Middle Char

---
+++ src/middlecharacter/MiddleChar2.java
@@ -0,0 +1,18 @@
+package middlecharacter;
+
+public class MiddleChar2 {
+
+    public String solution(String s) {
+        return s.substring((s.length() - 1) / 2, s.length()/2+1);
+    }
+
+    public static void main(String[] args) {
+
+        String s = "qwer";
+        MiddleChar2 middleChar =new MiddleChar2();
+        String solution = middleChar.solution(s);
+
+        System.out.println("===answer===");
+        System.out.println(solution);
+    }
+}
