1433a6f89c0642550b3c45521838885be4ad81f3
bsh6463 <bsh9510@gmail.com>
2021-12-12 14:47:09 +0900

    211212 Middle Char

---
+++ src/middlecharacter/MiddleChar.java
@@ -0,0 +1,23 @@
+package middlecharacter;
+
+public class MiddleChar {
+
+    public String solution(String s) {
+        if(s.length() % 2 != 0){
+            return  s.substring((s.length()-1) / 2, (s.length()-1)/2 + 1);
+
+        }else {
+            return  s.substring((s.length()-1) / 2, (s.length()-1)/2+ 2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String s = "qwer";
+        MiddleChar middleChar =new MiddleChar();
+        String solution = middleChar.solution(s);
+
+        System.out.println("===answer===");
+        System.out.println(solution);
+    }
+}
